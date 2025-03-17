6112ac41693452838104df5f2e665b45117ec216
Simon Moreau <simonmoreau.ndlumbres@gmail.com>
2025-03-17 11:31:35 +0100

    Première version de Minutes

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static (int, int) ConvertMinutes(int nbMin)
+    {
+        int hours = nbMin / 60;
+        int minutes = nbMin % 60;
+        return (hours, minutes);
+    }
+
+    static void Main()
+    {
+        var result = ConvertMinutes(130);
+        Console.WriteLine($"{result.Item1}:{result.Item2}");
+    }
+}
